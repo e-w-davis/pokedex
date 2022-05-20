a0f573b63d7f2f3270559425df8e1248c8c6b185
Eric W. Davis <e.w.davis15@gmail.com>
2022-05-20 14:37:33 -0400

    Get and Show functions working

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 
 const express = require("express");
 const app = express();
-
+const poke = require('./models/pokemon.js')
+const morgan = require('morgan');
+const methodOverride = require("method-override");
 
 // initialize express framework
 
@@ -13,14 +15,15 @@ const port = 3000;
 
 
 // mount our middleware
-
-
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(methodOverride("_method"))
 
 // mount our routes
 
 //I(ndex)
 app.get("/pokemon", (req, res) => {
-    res.send("Hi")
+    res.render('index.ejs', { poke: poke });
 });
 
 //N(ew)
@@ -34,9 +37,12 @@ app.get("/pokemon", (req, res) => {
 //E(dit)
 
 //S(how)
-app.get("/pokemon/:pokeIndex", () => {
-    
+app.get("/pokemon/:id", (req, res) => {
+    res.render('show.ejs', {
+        poke: poke[req.params.id],
+    });
 });
+
 // tell application to listen for requests
 
 app.listen(port, () => {
